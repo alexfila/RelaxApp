2a251448cdec0670c8d49efe43da12b08fe01f95
Alex Fila <fialex66@gmail.com>
2024-10-02 17:05:09 +0200

    Initial Commit

---
+++ RelaxApp/RelaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelaxAppApp.swift
+//  RelaxApp
+//
+//  Created by Alex Fila on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RelaxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
